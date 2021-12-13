0194cdb5931f2e25b8a72ffeeeb2d086b737c5be
Kyberspace <89725290+Dorabotmaster@users.noreply.github.com>
2021-12-13 22:19:57 +0530

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,22 @@
+termux-setup-storage
+apt install git
+apt install wget
+apt install python2
+apt install python2
+apt install python2
+apt install pip
+apt install sudo
+apt install tsu
+pkg upgrade && pkg install git
+git clone --depth=1 https://github.com/adi1090x/termux-desktop.git
+cd termux-desktop
+chmod +x setup.sh
+./setup.sh --install
+startdesktop
+apt update && apt upgrade
+pkg install python2 -y
+pkg install git -y
+git clone https://github.com/MaulanaRyM/FakeRoot
+cd FakeRoot
+
+python2 root.py
